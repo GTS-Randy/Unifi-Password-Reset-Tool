78782716c7cce940a862991e981a528f583f4c03
GTS-Randy <77634483+GTS-Randy@users.noreply.github.com>
2022-04-07 15:17:44 -0400

    Initial Seed

---
+++ Unifi Password Reset tool/frm_Main.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_Main))
+        Me.lstAccounts = New System.Windows.Forms.ListBox()
+        Me.lblUsername = New System.Windows.Forms.Label()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.lblEmail = New System.Windows.Forms.Label()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.btn_Reset = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstAccounts
+        '
+        Me.lstAccounts.FormattingEnabled = True
+        Me.lstAccounts.ItemHeight = 15
+        Me.lstAccounts.Location = New System.Drawing.Point(12, 12)
+        Me.lstAccounts.Name = "lstAccounts"
+        Me.lstAccounts.Size = New System.Drawing.Size(120, 94)
+        Me.lstAccounts.TabIndex = 0
+        '
+        'lblUsername
+        '
+        Me.lblUsername.AutoSize = True
+        Me.lblUsername.Location = New System.Drawing.Point(138, 12)
+        Me.lblUsername.Name = "lblUsername"
+        Me.lblUsername.Size = New System.Drawing.Size(60, 15)
+        Me.lblUsername.TabIndex = 1
+        Me.lblUsername.Text = "Username"
+        '
+        'txtUsername
+        '
+        Me.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtUsername.Location = New System.Drawing.Point(138, 30)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.ReadOnly = True
+        Me.txtUsername.Size = New System.Drawing.Size(60, 16)
+        Me.txtUsername.TabIndex = 2
+        '
+        'txtEmail
+        '
+        Me.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtEmail.Location = New System.Drawing.Point(204, 30)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.ReadOnly = True
+        Me.txtEmail.Size = New System.Drawing.Size(206, 16)
+        Me.txtEmail.TabIndex = 4
+        '
+        'lblEmail
+        '
+        Me.lblEmail.AutoSize = True
+        Me.lblEmail.Location = New System.Drawing.Point(204, 12)
+        Me.lblEmail.Name = "lblEmail"
+        Me.lblEmail.Size = New System.Drawing.Size(36, 15)
+        Me.lblEmail.TabIndex = 3
+        Me.lblEmail.Text = "Email"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtPassword.Location = New System.Drawing.Point(138, 83)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.ReadOnly = True
+        Me.txtPassword.Size = New System.Drawing.Size(272, 16)
+        Me.txtPassword.TabIndex = 6
+        Me.txtPassword.Visible = False
+        '
+        'lblPassword
+        '
+        Me.lblPassword.AutoSize = True
+        Me.lblPassword.Location = New System.Drawing.Point(138, 65)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(57, 15)
+        Me.lblPassword.TabIndex = 5
+        Me.lblPassword.Text = "Password"
+        '
+        'btn_Reset
+        '
+        Me.btn_Reset.Location = New System.Drawing.Point(220, 83)
+        Me.btn_Reset.Name = "btn_Reset"
+        Me.btn_Reset.Size = New System.Drawing.Size(75, 23)
+        Me.btn_Reset.TabIndex = 7
+        Me.btn_Reset.Text = "Reset"
+        Me.btn_Reset.UseVisualStyleBackColor = True
+        '
+        'frm_Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(419, 118)
+        Me.Controls.Add(Me.btn_Reset)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.lblEmail)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Controls.Add(Me.lblUsername)
+        Me.Controls.Add(Me.lstAccounts)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frm_Main"
+        Me.Text = "Unifi Controller Password Reset Tool"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lstAccounts As ListBox
+    Friend WithEvents lblUsername As Label
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents lblEmail As Label
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents lblPassword As Label
+    Friend WithEvents btn_Reset As Button
+End Class
---
+++ Unifi Password Reset tool/frm_Main.vb
@@ -0,0 +1,256 @@
+﻿Imports MongoDB.Driver
+Imports MongoDB.Bson
+Imports System.Text
+Imports System.Security.Cryptography
+Imports CryptSharp
+
+Public Class frm_Main
+
+    Const HashedPassword = "$6$ybLXKYjTNj9vv$dgGRjoXYFkw33OFZtBsp1flbCpoFQR7ac8O0FrZixHG.sw2AQmA5PuUbQC/e5.Zu.f7pGuF7qBKAfT/JRZFk8/"
+    Private server As New MongoServerAddress("Localhost", 27117)
+    Private dbsettings As New MongoClientSettings
+    Private client As MongoClient
+    Private crypto As New CryptSystem
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ' Hased Password: 
+        ' "password" = "$6$ybLXKYjTNj9vv$dgGRjoXYFkw33OFZtBsp1flbCpoFQR7ac8O0FrZixHG.sw2AQmA5PuUbQC/e5.Zu.f7pGuF7qBKAfT/JRZFk8/"
+        lstAccounts.Items.Clear()
+        For Each acct As UserAccount In Read_DB()
+            lstAccounts.Items.Add(acct)
+        Next
+    End Sub
+
+    Private Function Read_DB() As List(Of UserAccount)
+        dbsettings.Server = server
+        dbsettings.DirectConnection = True
+        client = New MongoClient(dbsettings)
+        Dim db As MongoDatabaseBase = client.GetDatabase("ace")
+        Dim col = db.ListCollectionNames()
+        Dim Collection As IMongoCollection(Of BsonDocument) = db.GetCollection(Of BsonDocument)("admin")
+        Dim q = New BsonDocument
+        Dim f = Builders(Of BsonDocument).Projection.Include("_id")
+        Dim accts As List(Of BsonDocument) = Collection.Find(q).ToList
+        Dim AdminAccounts As New List(Of UserAccount)
+        For Each acct As BsonDocument In accts
+            'If acct.Item("name") = "Admin" Or acct.Item("name") = "admin" Then
+            'Return New UserAccount(acct.Item("_id").ToString, acct.Item("name").ToString, acct.Item("email").ToString, acct.Item("x_shadow").ToString, acct.Item("time_created").ToString, acct.Item("last_site_name").ToString)
+            'End If
+            AdminAccounts.Add(New UserAccount(acct.Item("_id").ToString, acct.Item("name").ToString, acct.Item("email").ToString, acct.Item("x_shadow").ToString, acct.Item("time_created").ToString, acct.Item("last_site_name").ToString))
+        Next
+
+        Return AdminAccounts
+    End Function
+
+    Private Sub Update_Password()
+        dbsettings.Server = server
+        dbsettings.DirectConnection = True
+        client = New MongoClient(dbsettings)
+        Dim db As MongoDatabaseBase = client.GetDatabase("ace")
+        Dim col = db.ListCollectionNames()
+        Dim userCollection As IMongoCollection(Of BsonDocument) = db.GetCollection(Of BsonDocument)("admin")
+        Dim q = New BsonDocument
+        Dim accts As List(Of BsonDocument) = userCollection.Find(q).ToList
+        Dim filterByname = Builders(Of BsonDocument).Filter.Eq(Of String)("name", "Admin")
+        'userCollection.UpdateOne(filterByname, New BsonDocument("$set", New BsonDocument("password", HashedPassword)))
+        'For Each acct As BsonDocument In accts
+        userCollection.UpdateOne(Builders(Of BsonDocument).Filter.Eq(Of String)("name", lstAccounts.SelectedItem.UserName), New BsonDocument("$set", New BsonDocument("x_shadow", HashedPassword)))
+        MsgBox($"Password for { lstAccounts.SelectedItem.UserName } set to `password`", MsgBoxStyle.OkOnly, "Password Reset")
+        'Next
+    End Sub
+
+
+    Private Sub lstAccounts_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstAccounts.SelectedIndexChanged
+        Dim UsrAccount As UserAccount = lstAccounts.SelectedItem
+        txtEmail.Text = UsrAccount.Email
+        txtUsername.Text = UsrAccount.UserName
+        txtPassword.Text = UsrAccount.Password
+    End Sub
+
+    Private Sub txtPassword_DoubleClick(sender As Object, e As EventArgs) Handles txtPassword.DoubleClick
+        txtPassword.ReadOnly = False
+    End Sub
+
+    Private Sub txtPassword_KeyDown(sender As Object, e As KeyEventArgs) Handles txtPassword.KeyDown
+        If e.KeyCode = Keys.Enter Then txtPassword.ReadOnly = True
+    End Sub
+
+    Private Sub btn_Reset_Click(sender As Object, e As EventArgs) Handles btn_Reset.Click
+        Update_Password()
+    End Sub
+End Class
+
+Public Class UserAccount
+
+    Private _ID As String
+    Private _Name As String
+    Private _Email As String
+    Private _Password As String
+    Private _CreatedTime As String
+    Private _LastSite As String
+
+    Public ReadOnly Property ID As String
+        Get
+            Return _ID
+        End Get
+    End Property
+    Public ReadOnly Property UserName As String
+        Get
+            Return _Name
+        End Get
+    End Property
+    Public ReadOnly Property Email As String
+        Get
+            Return _Email
+        End Get
+    End Property
+    Public ReadOnly Property Password As String
+        Get
+            Return _Password
+        End Get
+    End Property
+    Public ReadOnly Property Last_Site As String
+        Get
+            Return _LastSite
+        End Get
+    End Property
+
+    Sub New()
+
+    End Sub
+    Sub New(Name As String, Email As String)
+        _Name = Name
+        _Email = Email
+    End Sub
+    Sub New(Name As String, Email As String, Password As String)
+        _Name = Name
+        _Email = Email
+        _Password = Password
+    End Sub
+    Sub New(ID As String, Name As String, Email As String, Password As String)
+        _ID = ID
+        _Name = Name
+        _Email = Email
+        _Password = Password
+    End Sub
+    Sub New(ID As String, Name As String, Email As String, Password As String, Time_Created As String, Last_Site As String)
+        _ID = ID
+        _Name = Name
+        _Email = Email
+        _Password = Password
+        _CreatedTime = Time_Created
+        _LastSite = Last_Site
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return _Name
+    End Function
+End Class
+
+Class CryptSystem
+    ' quickhash.com
+    ' SHA-512 / crypt(3) / $6$
+    Private Const OLDSALT As String = "9Ter1EZ9$lSt6"
+    Private Const NEWSALT As String = "4NfmBk8A"
+
+    Dim test As SHA512
+
+    Sub New()
+
+    End Sub
+
+    Public Function computeNewHash(ByVal clearText As String) As Byte()
+
+        Dim encoder As New Text.UTF8Encoding()
+        Dim sha512hasher As New System.Security.Cryptography.SHA512Managed()
+        Return sha512hasher.ComputeHash(encoder.GetBytes(clearText & NEWSALT))
+
+    End Function
+
+    Public Function GenerateNewSaltedHash(plainText As String) As Byte()
+        Dim algorithm As HashAlgorithm = New SHA256Managed()
+        Dim plainTextWithSaltBytes As Byte() = Encoding.ASCII.GetBytes(plainText & NEWSALT)
+
+        Return algorithm.ComputeHash(plainTextWithSaltBytes)
+    End Function
+
+    Public Function Generate_SHA512_Hash(ClearText As String, Optional salt As String = "abc") As String
+        'Dim SHACrypter As ShaCrypter
+        'Dim Sha512Crypter As Sha512Crypter
+
+
+        'ShaCrypter.Crypt("Linux8902*")
+        'Sha512Crypter.GetCrypter("$6$4NfmBk8A$S.qWAuFYSZ2aPyNesZS8Wq97fNt8H8NR.cmUH7rZ6qeC7KWU3PRuAJsrlnrrvZU6z5K3xdbo0O2yIMYqIciX40")
+        'Sha512Crypter.Crypt("Linux8902*")
+        Dim sha512C As Sha512Crypter = New Sha512Crypter
+        Dim opt As New CrypterOptions
+
+        'Dim test = sha512C.Crypt(ClearText, salt)
+
+        Return sha512C.Crypt(ClearText, Crypter.Blowfish.GenerateSalt())
+    End Function
+
+
+
+
+End Class
+
+Class RNGCSP
+    Private Shared rngCsp As New RNGCryptoServiceProvider()
+    ' Main method.
+    Public Shared Sub Main()
+        Const totalRolls As Integer = 25000
+        Dim results(5) As Integer
+
+        ' Roll the dice 25000 times and display
+        ' the results to the console.
+        Dim x As Integer
+        For x = 0 To totalRolls
+            Dim roll As Byte = RollDice(System.Convert.ToByte(results.Length))
+            results((roll - 1)) += 1
+        Next x
+        Dim i As Integer
+
+        While i < results.Length
+            Console.WriteLine("{0}: {1} ({2:p1})", i + 1, results(i), System.Convert.ToDouble(results(i)) / System.Convert.ToDouble(totalRolls))
+            i += 1
+        End While
+        rngCsp.Dispose()
+    End Sub
+
+
+    ' This method simulates a roll of the dice. The input parameter is the
+    ' number of sides of the dice.
+    Public Shared Function RollDice(ByVal numberSides As Byte) As Byte
+        If numberSides <= 0 Then
+            Throw New ArgumentOutOfRangeException("NumSides")
+        End If
+        ' Create a byte array to hold the random value.
+        Dim randomNumber(0) As Byte
+        Do
+            ' Fill the array with a random value.
+            rngCsp.GetBytes(randomNumber)
+        Loop While Not IsFairRoll(randomNumber(0), numberSides)
+        ' Return the random number mod the number
+        ' of sides.  The possible values are zero-
+        ' based, so we add one.
+        Return System.Convert.ToByte(randomNumber(0) Mod numberSides + 1)
+
+    End Function
+
+
+    Private Shared Function IsFairRoll(ByVal roll As Byte, ByVal numSides As Byte) As Boolean
+        ' There are MaxValue / numSides full sets of numbers that can come up
+        ' in a single byte.  For instance, if we have a 6 sided die, there are
+        ' 42 full sets of 1-6 that come up.  The 43rd set is incomplete.
+        Dim fullSetsOfValues As Integer = [Byte].MaxValue / numSides
+
+        ' If the roll is within this range of fair values, then we let it continue.
+        ' In the 6 sided die case, a roll between 0 and 251 is allowed.  (We use
+        ' < rather than <= since the = portion allows through an extra 0 value).
+        ' 252 through 255 would provide an extra 0, 1, 2, 3 so they are not fair
+        ' to use.
+        Return roll < numSides * fullSetsOfValues
+
+    End Function 'IsFairRoll
+End Class
